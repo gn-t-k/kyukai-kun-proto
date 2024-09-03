ebe72a752c50cdc7c2743904584bda407c41ef50
gn-t-k <gntk1995422@gmail.com>
2024-09-03 16:00:49 +0900

    github actionsのための設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+// 参照: https://github.com/brookslybrand/remix-gh-pages/blob/main/vite.config.ts
+
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const basename = "/kyukai-kun-proto/";
+
 export default defineConfig({
+  base: basename,
   plugins: [
     remix({
       future: {
@@ -11,6 +18,18 @@ export default defineConfig({
         v3_throwAbortReason: true,
       },
       ssr: false,
+      basename,
+      buildEnd: ({ viteConfig }) => {
+        if (!viteConfig.isProduction) {
+          return;
+        }
+
+        const buildPath = viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
     }),
     tsconfigPaths(),
   ],
